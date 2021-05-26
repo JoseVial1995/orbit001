f05db7a0097adad4a8d2d4490b29721f1baa9d7b
Jose Vial <jose.vial@routinguc.com>
2021-05-26 18:13:45 -0400

    complete user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
+  end
+  def show
+  	@user = User.find(params[:id])
+  end
+  def create
+  	#@user = User.new(params[:user])
+  	@user = User.new(name: params[:user][:name], email: params[:user][:email], password: params[:user][:password], password_confirmation: params[:user][:password_confirmation])
+  	if @user.save
+      flash[:success] = "Welcome to Orbit"
+  		redirect_to @user
+      #render 'show'
+  	else
+  		render 'new'
+  	end
   end
 end
